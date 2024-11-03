7d2d09d4b022c85813393397d170e6b8dc50feeb
ariichi88 <ariichi88@gmail.com>
2024-11-03 19:39:54 +0900

    修正

--- backup.sh
+++ backup.sh
@@ -11,11 +11,11 @@ dirs=(Documents Music Photo Pictures Videos)
 
 if [ -e $baklog ]; then
 	for dir in ${dirs[@]}; do
+		rsync -av --delete $dir $bakdir >> $baklog
+	done
+else
+	for dir in ${dirs[@]}; do
 		rsync -a $dir $bakdir
 	done
 	touch $baklog 
-else
-	for dir in ${dirs[@]}; do
-		rsync -av --delete $dir $bakdir >> $baklog
-	done
 fi
